eac499e9aeda0b38856a6ce1947e5dd6eff26053
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-06 13:16:55 +0545

    Mapped add & set up edit & delete

--- src/main/java/com/cibt/crm/controller/EnquirySourceController.java
+++ src/main/java/com/cibt/crm/controller/EnquirySourceController.java
@@ -6,11 +6,15 @@
 package com.cibt.crm.controller;
 
 import com.cibt.crm.base.SiteController;
+import com.cibt.crm.entity.EnquirySource;
 import com.cibt.crm.repository.EnquirySourceRepository;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -33,9 +37,31 @@ public class EnquirySourceController extends SiteController {
         model.addAttribute("records", repository.findAll());
         return "enquiry/source/index";
     }
-    
+
     @GetMapping(value = "/add")
     public String add() {
         return "enquiry/source/add";
     }
+
+    @PostMapping(value = "/add")
+    public String save(EnquirySource model) {
+        repository.save(model);
+        return "redirect:/enquiry/sources?success";
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("record", repository.getOne(id));
+        return "enquiry/source/edit";
+    }
+    
+    @GetMapping(value = "/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        EnquirySource source=repository.getOne(id);
+        source.setDeleted(true);
+        source.setDeletedDate(new Date());
+        repository.save(source);
+        return "redirect:/enquiry/sources?success";
+    }
+
 }
